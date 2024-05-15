230a158692ec65c771d3acc402635bcc6d71d867
Erik Zapata <zapatabrandon@gmail.com>
2024-05-14 20:29:10 -0400

    Init Commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use bevy::prelude::*;
+
+mod splash;
+
+#[derive(Clone, Copy, Default, Eq, PartialEq, Debug, Hash, States)]
+enum GameState {
+    #[default]
+    Splash,
+    Menu,
+    Game
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .init_state::<GameState>()
+        .add_systems(Startup, setup)
+        .add_plugins(splash::splash_plugin)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+fn despawn_screen<T: Component>(to_despawn: Query<Entity, With<T>>, mut commands: Commands) {
+    for entity in &to_despawn {
+        commands.entity(entity).despawn_recursive();
+    }
+}
